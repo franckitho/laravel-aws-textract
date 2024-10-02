726cea76ca2e3e838ba4d407a479da88d0998235
franckitho <noxyzdisign@gmail.com>
2024-10-02 23:31:43 +0200

    🚧 Add AnalyseDocument class and AnalyseDocument facade

---
+++ src/AnalyseDocument.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Franckitho\Textract;
+
+use Aws\Textract\TextractClient;
+
+class AnalyseDocument
+{
+    private TextractClient $client;
+    private String | array $features;
+    private String $bytes;
+
+    public function __construct()
+    {
+        $this->client = new TextractClient(
+            [
+                'region' => config('aws-textract.region'),
+                'version' => config('aws-textract.version'),
+                'credentials' => [
+                    'key' => config('aws-textract.credentials.key'),
+                    'secret' => config('aws-textract.credentials.secret'),
+                ],
+            ]
+        );
+    }
+
+    /**
+     * Set the features for the document analysis.
+     *
+     * @param string|array $features The features to be set for the analysis. It can be a single feature as a string or multiple features as an array.
+     * @return $this
+     */
+    public function features(String | array $features)
+    {
+        $this->features = $features;
+        return $this;
+    }
+
+    /**
+     * Sets the bytes property.
+     *
+     * @param string $bytes The byte data to be set.
+     * @return $this
+     */
+    public function file(String $bytes)
+    {
+        $this->bytes = $bytes;
+        return $this;
+    }
+
+    public function analyze()
+    {
+        return $this->client->analyzeDocument([
+            'FeatureTypes' => $this->features,
+            'Document' => [
+                'Bytes' => file_get_contents($this->bytes),
+            ],
+        ]);
+    }
+}
--- src/TextractServiceProvider.php
+++ src/TextractServiceProvider.php
@@ -4,22 +4,17 @@ namespace Franckitho\Textract;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Franckitho\Textract\Commands\TextractCommand;
 
 class TextractServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('laravel-aws-textract')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_laravel_aws_textract_table')
-            ->hasCommand(TextractCommand::class);
+            ->hasConfigFile('aws-textract');
+
+        $this->app->bind('analyseDocument', function () {
+            return new AnalyseDocument();
+        });
     }
 }
