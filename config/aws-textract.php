3fdad25e3b3fa91e986dfa46e1e5a4d99a0f1a2e
franckitho <noxyzdisign@gmail.com>
2024-10-02 23:31:00 +0200

    🔧 Update AWS Textract configuration

--- config/aws-textract.php
+++ config/aws-textract.php
@@ -2,5 +2,10 @@
 
 // config for Franckitho/Textract
 return [
-
+    'region' => env('AWS_REGION', 'us-east-1'),
+    'version' => env('AWS_TEXTRACT_VERSION', 'latest'),
+    'credentials' => [
+        'key' => env('AWS_ACCESS_KEY_ID'),
+        'secret' => env('AWS_SECRET_ACCESS_KEY'),
+    ],
 ];
