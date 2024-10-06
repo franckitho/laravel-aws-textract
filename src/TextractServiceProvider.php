d5649e3d24aa7ae7479cc808d3fb3a5d5ce6c99a
franckitho <franckitho@users.noreply.github.com>
2024-10-06 19:04:56 +0000

    🚨 Fix styling

--- src/TextractServiceProvider.php
+++ src/TextractServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace Franckitho\Textract;
 
-use Franckitho\Textract\Commands\TextractCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
